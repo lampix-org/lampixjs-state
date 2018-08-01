f3f7b0c87613969d4f86058c7461f101c45b525c
Dan Balaban <dbalaban@lampix.co>
2018-08-01 14:38:44 +0300

    Updated library starter to include CSS files in a bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
+import css from 'rollup-plugin-css-only';
 
 const ts2 = require('typescript');
 
@@ -28,8 +29,8 @@ export default [
         typescript: ts2
       }),
       resolve(),
-      commonjs()
-    ],
-    external: defaults.external
+      commonjs(),
+      css({ output: 'lib/styles.css' })
+    ]
   }
 ]
